771e50083bab207db75c7e6648a737e46a1075e1
tariqmuhanna <32185606+tariqmuhanna@users.noreply.github.com>
2019-02-09 19:50:00 -0600

    added some more functions and lec code to OS.c

--- Lab 2/OS.c
+++ Lab 2/OS.c
@@ -8,8 +8,34 @@
 #include "../inc/tm4c123gh6pm.h"
 #include "OS.h"
 
+int32_t StartCritical(void);
+void EndCritical(int32_t primask);
+
 #define PF1         (*((volatile uint32_t *)0x40025008))
-	
+#define NUMTHREADS 30
+#define STACKSIZE   100      // number of 32-bit words in stack
+static uint32_t num_threads = 0;
+
+
+struct TCB {
+	int32_t *savedSP;
+	unsigned used;
+	struct TCB *next;
+  struct TCB *prev;
+	int16_t id;
+	uint16_t sleep;
+	uint8_t priority;
+	uint8_t blocked;
+};
+typedef struct TCB TCBtype;
+
+TCBtype *runPT = 0;
+TCBtype TCBs[NUMTHREADS];
+unsigned int numTasks = 0;
+int16_t CurrentID;
+int32_t Stacks[NUMTHREADS][STACKSIZE];
+
+
 void (*PeriodicTask)(void);   // user function
 uint32_t OS_counter = 0;
 
@@ -43,6 +69,159 @@ void Timer5A_Handler(void){
   (*PeriodicTask)();                 // Execute periodic task
 	PF1 = 0x0;                         // Signal for exiting the interrupt handler
 }
+
+//long* OS_initStack(long *sp, void(*task)(void)){
+//	*(sp)   = (long)task;
+//	*(--sp) = (long)0x12121212L;
+//  *(--sp) = (long)0x11111111L;
+//  *(--sp) = (long)0x10101010L;
+//  *(--sp) = (long)0x09090909L;
+//  *(--sp) = (long)0x08080808L;
+//  *(--sp) = (long)0x07070707L;
+//  *(--sp) = (long)0x06060606L;
+//  *(--sp) = (long)0x05050505L;
+//  *(--sp) = (long)0x04040404L;
+//  *(--sp) = (long)0x03030303L;
+//  *(--sp) = (long)0x02020202L;
+//  *(--sp) = (long)0x01010101L;
+//  *(--sp) = (long)0x00000000L;
+
+//	return sp;
+//}
+
+void SetInitialStack(int i){
+  TCBs[i].savedSP = &Stacks[i][STACKSIZE-16]; // thread stack pointer
+  Stacks[i][STACKSIZE-1] = 0x01000000;   // thumb bit
+  Stacks[i][STACKSIZE-3] = 0x14141414;   // R14
+  Stacks[i][STACKSIZE-4] = 0x12121212;   // R12
+  Stacks[i][STACKSIZE-5] = 0x03030303;   // R3
+  Stacks[i][STACKSIZE-6] = 0x02020202;   // R2
+  Stacks[i][STACKSIZE-7] = 0x01010101;   // R1
+  Stacks[i][STACKSIZE-8] = 0x00000000;   // R0
+  Stacks[i][STACKSIZE-9] = 0x11111111;   // R11
+  Stacks[i][STACKSIZE-10] = 0x10101010;  // R10
+  Stacks[i][STACKSIZE-11] = 0x09090909;  // R9
+  Stacks[i][STACKSIZE-12] = 0x08080808;  // R8
+  Stacks[i][STACKSIZE-13] = 0x07070707;  // R7
+  Stacks[i][STACKSIZE-14] = 0x06060606;  // R6
+  Stacks[i][STACKSIZE-15] = 0x05050505;  // R5
+  Stacks[i][STACKSIZE-16] = 0x04040404;  // R4
+}
+
+// 1 = free 
+// 0 = busy
+int add_thread() {
+	// loop through all threads to find one that is free
+	for (int i=0; i < NUMTHREADS; i++) {
+	  // if the thread is free, set it to work
+		if (TCBs[i].used) {
+				TCBs[i].used = 0; // set to busy
+				return i;
+		}
+	}
+	// if no threads are free, return -1
+	return -1; 
+}
+
+
+int find_prev(int thread) {
+  // loop through all threads in forward direction 
+	// to find one that is busy/alive   
+	for (int i = (thread+NUMTHREADS-1)%NUMTHREADS; i != thread; i = (i+NUMTHREADS-1)%NUMTHREADS ) {
+		if (!TCBs[i].used) 
+				return i;
+	}
+	// if error, return -1
+	return -1;
+}
+
+
+int find_next(int thread) {
+	// loop through all threads in forward direction 
+	// to find one that is busy/alive
+	for (int i = (thread+1)%NUMTHREADS; i != thread; i = (i+1)%NUMTHREADS ) {
+		if (!TCBs[i].used)
+				return i;
+	}
+	// if error, return -1
+	return -1;
+}
+
+//******** OS_AddThread *************** 
+// add a foregound thread to the scheduler
+// Inputs: pointer to a void/void foreground task
+//         number of bytes allocated for its stack
+//         priority, 0 is highest, 5 is the lowest
+// Outputs: 1 if successful, 0 if this thread can not be added
+// stack size must be divisable by 8 (aligned to double word boundary)
+// In Lab 2, you can ignore both the stackSize and priority fields
+// In Lab 3, you can ignore the stackSize fields
+int OS_AddThread(void(*task)(void), 
+   unsigned long stackSize, unsigned long priority){
+
+//void OS_AddThread(long *sp, void (*task)(void)){
+//	TCBtype *tcb, *last;
+//	tcb = &TCBs[numTasks++];
+//	tcb->savedSP = OS_initStack(sp, task);
+//	tcb->used = 1;
+
+//	if(runPT){
+//		last = runPT;
+//		while(last->next != runPT) last = last->next;
+//		last->next = tcb;
+//		tcb->next = runPT;
+//	} else {
+//		runPT = tcb;
+//		tcb->next = tcb;
+//	}
+//	
+	int32_t status, thread, prev;
+	 
+	status = StartCritical();
+	if(num_threads == 0) {
+		add_thread();
+		TCBs[0].next = &TCBs[0]; // 0 points to 0
+		runPT = &TCBs[0];     // thread 0 will run first
+	}
+	else {
+		thread = add_thread();
+		prev = find_prev(thread);
+		TCBs[thread].next = TCBs[prev].next;
+		TCBs[prev].next = &TCBs[thread];
+	}
+  TCBs[thread].used = 1;
+	TCBs[thread].id = thread;
+
+	SetInitialStack(thread); 
+	Stacks[thread][STACKSIZE-2] = (int32_t)(task); // PC
+	num_threads++;
+
+	SetInitialStack(thread);		//initialize stack
+	Stacks[num_threads][STACKSIZE-2] = (int32_t)(task); //  set PC for Task
+	EndCritical(status);
+
+	return 0;
+}
+
+
+
+
+void ContextSwitch(void){
+	NVIC_INT_CTRL_R = NVIC_INT_CTRL_PEND_SV;//NVIC_PENDSVSET; // lowest priority
+}
+
+void OS_Yield(void){
+	ContextSwitch();
+}
+
+void Sustick_handler(void) {
+	ContextSwitch();
+}
+
+void OS_launch(void){
+//	StartOS;
+}
+
 
 // ***************** OS_ClearPeriodicTime ****************
 // Resets global counter
@@ -98,17 +277,7 @@ void OS_bWait(Sema4Type *semaPt){}
 // output: none
 void OS_bSignal(Sema4Type *semaPt){} 
 	
-//******** OS_AddThread *************** 
-// add a foregound thread to the scheduler
-// Inputs: pointer to a void/void foreground task
-//         number of bytes allocated for its stack
-//         priority, 0 is highest, 5 is the lowest
-// Outputs: 1 if successful, 0 if this thread can not be added
-// stack size must be divisable by 8 (aligned to double word boundary)
-// In Lab 2, you can ignore both the stackSize and priority fields
-// In Lab 3, you can ignore the stackSize fields
-int OS_AddThread(void(*task)(void), 
-   unsigned long stackSize, unsigned long priority){return 0;}
+
 
 //******** OS_Id *************** 
 // returns the thread ID for the currently running thread
